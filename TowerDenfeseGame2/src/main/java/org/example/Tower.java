f3055601edc1c9f3a8c249e42ef68fdfed152256
Bre004 <altherego04@gmail.com>
2024-06-26 08:42:25 -0500

    Fin

---
+++ TowerDenfeseGame2/src/main/java/org/example/Tower.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Tower {
+    private char symbol;
+
+    public Tower(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
